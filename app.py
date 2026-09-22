7308e71f929983bd6107c5f812ab4f3f1a4536f7
Matthewh1225 <mhuckman97@yahoo.com>
2026-09-21 22:31:10 -0500

    added simple skeleton for multiple pages and implemted basic ai planner

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
 from routes.auth import auth_bp
+from routes.planner import planner_bp
+from routes.products import products_bp
+from routes.dashboard import dashboard_bp
+from Ratelimits  import limiter
 app = Flask(__name__, template_folder="pages")
-
+limiter.init_app(app)
+app.register_blueprint(auth_bp)
+app.register_blueprint(planner_bp)
+app.register_blueprint(products_bp)
+app.register_blueprint(dashboard_bp)
 
 @app.route("/")
 def home():
