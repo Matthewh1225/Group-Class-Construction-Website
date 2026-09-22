fd4f4f850f9b65b90c0a75ed5340f1f7fd7733ce
Matthewh1225 <mhuckman97@yahoo.com>
2026-09-22 13:06:53 -0500

    add live rate limiter via redis

--- Ratelimits.py
+++ Ratelimits.py
@@ -1,8 +1,18 @@
+import os
+from dotenv import load_dotenv
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+load_dotenv()
+
+#"memory://" used for localHost
 limiter=Limiter(
     key_func=get_remote_address,
-    storage_uri="memory://",
+    storage_uri=os.getenv(
+    "RATELIMIT_STORAGE_URI",
+        "memory://"
+    ),
     headers_enabled=True,
+    #improves time winmdow for ratelimit
+    strategy="moving-window",
 )
